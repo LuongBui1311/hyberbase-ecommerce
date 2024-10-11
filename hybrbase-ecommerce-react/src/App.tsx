23e771aab9d069be9fe9a565dc960dafe6c6bf85
Lương Bùi <luonglb1311@gmail.com>
2024-10-11 14:38:12 +0700

    Developed base UI for Product List

--- hybrbase-ecommerce-react/src/App.tsx
+++ hybrbase-ecommerce-react/src/App.tsx
@@ -1,12 +1,22 @@
-import Header from "./components/Header"
+import { Outlet } from "react-router-dom";
+import { Bounce, ToastContainer } from "react-toastify";
 
 function App() {
 
   return (
-    <>
-      <Header/>
-    </>
-  )
+    <div className="overflow-hidden text-base w-full bg-slate-50">
+      <ToastContainer
+        position="top-right"
+        transition={Bounce}
+        autoClose={2000}
+        draggable={true}
+        theme="colored"
+        hideProgressBar={true}
+        toastClassName="!font-sans !rounded-md"
+      />
+      <Outlet />
+    </div>
+  );
 }
 
 export default App
--- hybrbase-ecommerce-react/src/main.tsx
+++ hybrbase-ecommerce-react/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './output.css'
+// import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./output.css";
+import { RouterProvider } from "react-router-dom";
+import routes from "./routes/index.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")!).render(
+  // <StrictMode>
+  <RouterProvider router={routes} />
+  // </StrictMode>,
+);
